ed7daae5c7d062b5adb65ac55813cf77ab59e1ca
smcgov-nuigalway <stephenmcgov@hotmail.co.uk>
2021-06-27 03:24:10 +0100

    Update util.js
    DB details exposed, comment entry and require manual input, provide clarification

--- models/util.js
+++ models/util.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://mongodb4214ms:xi7fum@danu7.it.nuigalway.ie:8717/mongodb4214');
+//var connection = mongoose.connect('mongodb://mongodb4214ms:xi7fum@danu7.it.nuigalway.ie:8717/mongodb4214');
+//student DB acc, unsafe to use and will be rendered inoperative at some point, enter your own DB details here or setup a local instance
 
 exports.connection = connection;
 
