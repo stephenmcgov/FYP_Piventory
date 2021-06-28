aaeffc2c2666ecdf3ed4fa6d1bb4bf845515fe99
smcgov-nuigalway <stephenmcgov@hotmail.co.uk>
2021-06-28 13:41:46 +0100

    Update util.js

--- models/util.js
+++ models/util.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
-//var connection = mongoose.connect('mongodb://mongodb4214ms:xi7fum@danu7.it.nuigalway.ie:8717/mongodb4214');
-//student DB acc, unsafe to use and will be rendered inoperative at some point, enter your own DB details here or setup a local instance
+//var connection = mongoose.connect('your_DB_details_go_here');
 
 exports.connection = connection;
 
